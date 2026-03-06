109d835ade5c89478d339cd340b4ef909a67adba
Ritika Soni <ritikasoni6759@gmail.com>
2026-03-06 18:34:26 +0530

    Added Hello World C++ program

---
+++ basics/hello_world.cpp
@@ -0,0 +1,10 @@
+// Program: Hello World
+// Concept: Basic C++ program structure
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
