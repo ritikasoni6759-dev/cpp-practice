0cbad622bc88cef332acde87a1e06f35107c76e1
Ritika Soni <ritikasoni6759@gmail.com>
2026-03-06 18:36:36 +0530

    Added basic input and output program in C++

---
+++ basics/input_output.cpp
@@ -0,0 +1,11 @@
+// Program: Basic Input Output
+// Concept: Taking input and printing output in C++
+#include <iostream>
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c<<endl; 
+    return 0;
+}
