e584a71858202c4eb08b77c92836a1d30461c892
Ritika Soni <ritikasoni6759@gmail.com>
2026-03-11 20:44:40 +0530

    Added vector sort program

---
+++ stl/vector_sort.cpp
@@ -0,0 +1,29 @@
+// Platform: HackerRank
+// Problem: Vector-Sort
+// Topic: STL - vector and sorting
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n;
+    cin>>n;
+    
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    } 
+    
+    std::sort(v.begin(),v.end());
+    for(int i=0;i<n;i++)
+    {
+        cout<<v[i]<<" ";
+    } 
+    return 0;
+}
