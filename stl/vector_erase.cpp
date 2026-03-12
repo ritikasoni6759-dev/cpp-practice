f9e194bcb887eb5d5e97a224afbe7663d98d1832
Ritika Soni <ritikasoni6759@gmail.com>
2026-03-12 21:52:11 +0530

    Added vector erase program

---
+++ stl/vector_erase.cpp
@@ -0,0 +1,35 @@
+// Platform: HackerRank
+// Problem: Vector-Erase
+// Topic: STL vector erase
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    
+    for(int i=0;i<n;i++)
+    cin>>v[i];
+    
+    int x;
+    int a,b;
+     
+     cin>>x;
+     cin>>a>>b;
+    
+    v.erase(v.begin()+(x-1));
+    v.erase(v.begin()+(a-1),v.begin()+(b-1));
+    
+    cout<<v.size()<<endl;
+    for(int i=0;i<v.size();i++)
+    cout<<v[i]<<" ";
+    return 0;
+}
